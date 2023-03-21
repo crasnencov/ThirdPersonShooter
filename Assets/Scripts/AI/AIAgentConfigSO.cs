5bee436f6f6cf7b219f613a01a15657fa19aa888
Alexander Crasnencov <crasnencov@gmail.com>
2023-03-22 00:22:18 +0200

    state machine isnt working

---
+++ Assets/Scripts/AI/AIAgentConfigSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class AIAgentConfigSO : ScriptableObject
+{
+    public float maxTime = 1.0f;
+    public float maxDistance = 1.0f;
+}
